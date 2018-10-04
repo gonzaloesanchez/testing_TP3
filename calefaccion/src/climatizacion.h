7637911c729d56257c81c691c8091137659a547a
El_Gonza <gonzalo.e.sanchez@hotmail.com>
2018-10-03 21:11:33 -0300

    Test completo para requerimiento AUTO-ER-0001-REQ0018

--- calefaccion/src/climatizacion.h
+++ calefaccion/src/climatizacion.h
@@ -1,5 +1,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-void Climatizacion_create (bool *X);
+struct _climatizacionCtrl  {
+  bool *IO_ctrl;
+  uint8_t *temp_setting;
+};
+
+void Climatizacion_create (bool *IO, uint8_t *setTemp);
 void Climatizacion_OnOff (bool X);
+void Climatizacion_SetTemp (uint8_t temp);
--- calefaccion/test/test_climatizacion.c
+++ calefaccion/test/test_climatizacion.c
@@ -10,11 +10,15 @@ void tearDown (void)  {
 
 }
 
+/*****************************************************************************
+    TEST Correspondientes a requerimiento AUTO-ER-0001-REQ0019
+*******************************************************************************/
 void test_ClimatizacionOff(void)  {
    //creamos un control virtual de encendido_apagado
    bool on_off_virtual = false;
+   uint8_t selector_temp_virtual = 24;
 
-   Climatizacion_create(&on_off_virtual);
+   Climatizacion_create(&on_off_virtual, &selector_temp_virtual);
    Climatizacion_OnOff(true);
    TEST_ASSERT_EQUAL(true,on_off_virtual);
 }
@@ -22,8 +26,26 @@ void test_ClimatizacionOff(void)  {
 void test_ClimatizacionOn(void)  {
    //creamos un control virtual de encendido_apagado
    bool on_off_virtual = false;
+   uint8_t selector_temp_virtual = 24;
 
-   Climatizacion_create(&on_off_virtual);
+   Climatizacion_create(&on_off_virtual, &selector_temp_virtual);
    Climatizacion_OnOff(false);
    TEST_ASSERT_EQUAL(false,on_off_virtual);
 }
+//-----------------------------------------------------------------------------
+
+
+/*****************************************************************************
+    TEST Correspondientes a requerimiento AUTO-ER-0001-REQ0018
+*******************************************************************************/
+void test_ClimatizacionSetTemp(void)  {
+    //creamos un selector de temperatura virtual con valor por defecto
+    bool on_off_virtual = false;
+    uint8_t selector_temp_virtual = 24;
+
+    Climatizacion_create(&on_off_virtual, &selector_temp_virtual);
+    Climatizacion_SetTemp(26);
+    TEST_ASSERT_EQUAL_UINT8(26,selector_temp_virtual);
+}
+
+//-----------------------------------------------------------------------------
